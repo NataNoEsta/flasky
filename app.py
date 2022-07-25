0fd86269231d557098b051e51851531c2fabaf8a
natanatalina <shotaoouji@gmail.com>
2022-07-25 07:51:53 -0300

    modificaciones

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "abcd1234"
 
-@app.route("/hello")
+@app.route("/")
 def index():
-    flash("¿Cómo te llamas?")
+    flash("¡Hola! ¿Quién sos?")
     return render_template("index.html")
 
 @app.route("/greet", methods=["POST", "GET"])
 def greet():
     name = request.form["name_input"]
-    flash("Hola " + name.capitalize() + ", que bueno verte! ")
+    flash("Hola " + name.capitalize() + ", no molestes por favor! Besi!")
+    if len(name) > 10:
+        render_template("nomol.html")
     return render_template("index.html")
+
+@app.route("/nomol")
+def nomol():
+    return render_template("nomol.html")
