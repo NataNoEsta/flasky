ff31e74467851ff327ae4eed5bc8369044c93db3
natanatalina <shotaoouji@gmail.com>
2022-09-05 10:51:52 -0300

    update

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, flash
+from random import randint
 
 app = Flask(__name__)
 app.secret_key = "abcd1234"
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 @app.route("/")
 def index():
@@ -11,5 +15,14 @@ def index():
 @app.route("/greet", methods=["POST", "GET"])
 def greet():
     name = request.form["name_input"]
-    flash("Hola " + name.capitalize() + ", no molestes por favor! Besi!")
-    return render_template("index.html")
+    if name == 'Diego'.lower():
+        flash("Hola " + name.capitalize() + ", te extraño...")
+    else:
+        flash("Hola " + name.capitalize() + saludos())
+    return render_template("byebestie.html")
+
+def saludos():
+    var_saludos = [" no molestes plis!", " vete a la m1erda uwu", " cómo estás?", " quién sos?", " un gusto, besi!"]
+    limit = len(var_saludos)
+    x = randint(0, limit-1)
+    return var_saludos[x]
